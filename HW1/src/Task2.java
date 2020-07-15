96106edfbdeec499cfe4cd9c9a122fe323e51187
Alex Tutunik <alexratya@gmail.com>
2020-07-15 17:37:10 +0300

    added hw1

---
+++ HW1/src/Task2.java
@@ -0,0 +1,5 @@
+public class Task2 {
+	public static void main (String[] args) {
+		System.out.println(args[5] + " " + args[4] + " " + args[3] + " " + args[2] + " " + args[1] + " " + args[0]);
+	}
+}
---
+++ HW1/src/Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+
+    public static void main(String[] args) {
+        int num[] = {25, 17, 5, 62, 547, 68, 11, 28};
+
+        System.out.println(num[0] + " " + num[1] + " " + num[2] + " " + num[3] + " "
+                + num[4] + " " + num[5] + " " + num[6] + " " + num[7]);
+        System.out.println(num[0]);
+        System.out.println(num[1]);
+        System.out.println(num[2]);
+        System.out.println(num[3]);
+        System.out.println(num[4]);
+        System.out.println(num[5]);
+        System.out.println(num[6]);
+        System.out.println(num[7]);
+    }
+}
